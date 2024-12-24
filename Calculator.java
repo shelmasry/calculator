55f0413a8f2cc41e9b9293e763623b0429c2e752
shelmasry <shelmasry@gmail.com>
2024-12-24 13:13:59 +0200

    Adding a method for doubles b1

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,5 @@ public int addition(int a, int b) {
     return a + b;
 }
 }
-"public double addition(double a, double b) { return b + a ; } " 
-"public double addition_2(double a, double b) { return b + a ; } " 
-"public double addition_2(double a, double b) { return a + b ; } " 
+
+"public double addition_2(double a, double b) { return a + b1 ; } " 
