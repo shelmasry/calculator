1669608390554493e26b1bc39704b880bc58bd1f
shelmasry <shelmasry@gmail.com>
2024-12-24 15:36:00 +0200

    add subtraction method

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,10 @@ public class Calculator {
 public int addition(int a, int b) {
     return a + b;
 }
+public double addition_2(double a, double b) { return a + c ; }
+public int subtraction(int a, int b) {return a - b;}
+public double subtraction(double a, double b) {return a - b; }
 }
 
- 
-"public double addition_2(double a, double b) { return a + c ; } " 
+
+
